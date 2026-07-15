58e8340f8ce1c303427e35ac646609cf33a437ff
hedead0870-png <hedead0870@gmail.com>
2026-07-16 01:08:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import telebot
+from config import TOKEN
 
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Бот работает ✅")
+
+@bot.message_handler(func=lambda message: True)
+def echo(message):
+    bot.send_message(message.chat.id, message.text)
+
+bot.infinity_polling()
